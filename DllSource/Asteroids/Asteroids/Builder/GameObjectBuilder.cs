165307da3a001f1f794cce3ea3c5120804d4591e
Kkamikadzee <Kkamikadzee@yandex.ru>
2020-08-23 20:47:24 +0400

    Edited model
    Отредактировал модель. Изменения пока только в диаграмме.

--- DllSource/Asteroids/Asteroids/Builder/GameObjectBuilder.cs
+++ DllSource/Asteroids/Asteroids/Builder/GameObjectBuilder.cs
@@ -1,8 +1,11 @@
-﻿namespace KMK.Models.Builder
+﻿using KMK.Models.Base;
+
+namespace KMK.Models.Builder
 {
     public abstract class GameObjectBuilder
     {
         public virtual void BuildComponentsStorage() { }
+        public virtual void BuildComponentsStorage(Transform transform) { }
         public virtual void BuildSphereCollider() { }
         public virtual void BuildMover() { }
         public virtual void BuildMotionController() { }
